dbc395ccc5166eb395f4269d4155b0b85f51f614
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-18 13:55:48 +0300

    callback func

--- practice/script.js
+++ practice/script.js
@@ -124,6 +124,34 @@
 // console.log(parseInt(test));
 // console.log(parseFloat(test));
 
+//callback функции 
+ 
+function first() {
+    //do something
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2)
+}
+
+first();
+second();
+
+function learnJs(lang, callback) {
+    console.log(`Я учу ${lang}`)
+    callback();
+}
+
+learnJs('javascript', function(){
+    console.log('Я прошел этот урок!');
+});
+
+
+
+
 
  
 
