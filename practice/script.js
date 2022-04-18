826fb389d3c61e01d5fdeeb9a3dc7e5777fe6ade
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-18 12:43:48 +0300

    some method

--- practice/script.js
+++ practice/script.js
@@ -109,5 +109,21 @@ console.log(fruit.indexOf("fruit"))
 
 const logg = 'Hello world'
 
-console.log(logg.slice(6))
-console.log(logg.slice(-5, -1))
+console.log(logg.slice(6));
+console.log(logg.slice(-5, -1));
+
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px'
+console.log(parseInt(test));
+console.log(parseFloat(test));
+
+
+
+
