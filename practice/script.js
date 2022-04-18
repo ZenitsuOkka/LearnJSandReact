ffff4cda7565f9d0c170ff52eda4f4477d9c9986
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-18 17:21:09 +0300

    object method keys

--- practice/script.js
+++ practice/script.js
@@ -164,19 +164,22 @@ const options = {
     }
 };
 
-console.log(options.name)
+console.log(Object.keys(options))
 
-delete options.name
+// delete options.name
 
-console.log(options)
+// console.log(options)
+
 //for in конструкция
-for (let key in options) {
-    if (typeof(options[key]) === 'object'){
-        for (let i in options[key]) {
-            console.log(`свойство ${i} имеет значение ${options[key][i]}`)
-        }
-    } else {
-        console.log(`свойство ${key} имеет значение ${options[key]}`)
-    } 
-}
-
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object'){
+//         for (let i in options[key]) {
+//             console.log(`свойство ${i} имеет значение ${options[key][i]}`)
+//         }
+//     } else {
+//         console.log(`свойство ${key} имеет значение ${options[key]}`)
+//     } 
+//     counter++;
+// }
+// console.log(counter) 
