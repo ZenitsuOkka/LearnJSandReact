ffc1e00d58e1266fa6caaacaee908fe2989e4ac2
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-20 16:28:33 +0300

    massive tag button one element

--- web_practice/elements/js/script.js
+++ web_practice/elements/js/script.js
@@ -4,6 +4,6 @@ const box =  document.getElementById('box');
 
 console.log(box);
 
-const btns = document.getElementsByTagName("button");
+const btns = document.getElementsByTagName("button")[1];//получили псевдо массив
 
 console.log(btns);
