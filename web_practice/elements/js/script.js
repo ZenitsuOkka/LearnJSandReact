be2f15e95defdcd162f7fe092e54b1305bae6c86
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-20 19:53:00 +0300

    web practice 2

--- web_practice/elements/js/script.js
+++ web_practice/elements/js/script.js
@@ -1,24 +1,11 @@
 'use strict';
 
 const box =  document.getElementById('box');
-
-console.log(box);
-
-const btns = document.getElementsByTagName("button");//получили псевдо массив
-
-console.log(btns[1]);
-
-const circles = document.getElementsByClassName('circle');
-console.log(circles);
-
-const hearts = document.querySelectorAll(".heart");
-
-hearts.forEach(item => {
-    console.log(item);
-})
-
-const oneHeart = document.querySelector('.heart');//метод возвращает первый эелемент такого селектора
-
-console.log(oneHeart);
+      circles = document.getElementsByClassName('circle');
+      btns = document.getElementsByTagName("button");//получили псевдо массив
+      oneHeart = document.querySelector('.heart');//метод возвращает первый эелемент такого селектора
+      hearts = document.querySelectorAll(".heart");
+      
 //методы querySelector и getElementById возвращают один элемент
 //методы querySelectorAll, getElementsByTagName возвращают псевдомассив
+
