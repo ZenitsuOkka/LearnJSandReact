51e07e8d244fd408485b3e47d589bdfd85f1cc27
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-21 21:15:07 +0300

    method parentNode

--- navigation in DOM/script.js
+++ navigation in DOM/script.js
@@ -1,3 +1,7 @@
 // console.log(document.body);
 // console.log(document.documentElement);
-console.log(document.body.childNodes);
+// console.log(document.body.childNodes);//узлы которые являются детьми у body
+// console.log(document.body.firstChild);//
+// console.log(document.body.lastChild);//
+
+console.log(document.querySelector('#current').parentNode);
