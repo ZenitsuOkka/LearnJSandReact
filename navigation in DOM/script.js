4789ac97bfb96dc113c9425421752b8ade154770
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-22 21:04:44 +0300

    practice 2

--- navigation in DOM/script.js
+++ navigation in DOM/script.js
@@ -1,19 +1,19 @@
-// console.log(document.body);
-// console.log(document.documentElement);
-// console.log(document.body.childNodes);//узлы которые являются детьми у body
-// console.log(document.body.firstChild);//
-// console.log(document.body.lastChild);//
+    // console.log(document.body);
+    // console.log(document.documentElement);
+    // console.log(document.body.childNodes);//узлы которые являются детьми у body
+    // console.log(document.body.firstChild);//
+    // console.log(document.body.lastChild);//
 
-// console.log(document.querySelector('#current').parentNode.parentNode);//обертка для блоков
- 
-// console.log(document.querySelector('[data-current="3"]').nextSibling);
-// console.log(document.querySelector('[data-current="3"]').previousSibling);
-// console.log(document.querySelector('[data-current="3"]').previousElementSibling);
-// //все эти принципы 
+    // console.log(document.querySelector('#current').parentNode.parentNode);//обертка для блоков
+    
+    // console.log(document.querySelector('[data-current="3"]').nextSibling);
+    // console.log(document.querySelector('[data-current="3"]').previousSibling);
+    // console.log(document.querySelector('[data-current="3"]').previousElementSibling);
+    // //все эти принципы 
 
-for (let node of document.body.childNodes) {
-    if (node.nodeName == '#text') {
-        continue;
-    }
-    console.log(node);
-};
+    for (let node of document.body.childNodes) {
+        if (node.nodeName == '#text') {
+            continue;
+        }
+        console.log(node);
+    };
