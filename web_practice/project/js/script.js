80cc39ed6c067f3bcd598746e5682c1b8844f613
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-22 22:06:57 +0300

    simple recursion

--- web_practice/project/js/script.js
+++ web_practice/project/js/script.js
@@ -98,6 +98,8 @@ document.addEventListener('DOMContentLoaded', () => {
             btn.addEventListener('click', () => {
                 btn.parentElement.remove();
                 movieDB.movies.splice(i, 1);
+                createMovieList(movieDB.movies, movieList);
+                
             });
         });
     };
