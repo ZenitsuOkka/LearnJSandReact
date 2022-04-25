604365cc7ec4bfe26dcf3eb8f889c3ab237936a1
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-25 18:16:46 +0300

    animation

--- practice2/scriptAndTimes/script.js
+++ practice2/scriptAndTimes/script.js
@@ -2,21 +2,33 @@ const btn = document.querySelector('.btn');
 let timerId;  
     i = 0;
 
-btn.addEventListener('click', () => {
-    // const timeeId = setTimeout(logger, 2000);
-    timerId = setInterval(logger, 500);
 
-}); 
+function myAnimation() {
+    const elem = document.querySelector('.box');
+    let pos = 0;
 
-// clearInterval(timeeId); 
-
-function logger () {
-    if (i === 3) {
-        clearInterval(timerId);
+    const id = setInterval(frame, 10)
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
     }
-    console.log('Text')
-    i++;
 };
+btn.addEventListener('click', myAnimation);
+
+// // clearInterval(timeeId); 
+
+// function logger () {
+//     if (i === 3) {
+//         clearInterval(timerId);
+//     }
+//     console.log('Text')
+//     i++;
+// };
 
 // let id = setTimeout(function log() {
 //     console.log('hello');
