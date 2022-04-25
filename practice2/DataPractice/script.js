a073e09885ffe3ef812f67fd8de02d6c31fe6c77
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-25 19:34:49 +0300

    set method

--- practice2/DataPractice/script.js
+++ practice2/DataPractice/script.js
@@ -2,6 +2,20 @@
 
 const now =  new Date();
 
+// console.log(now)
+
+// console.log(now.getFullYear());
+
+// console.log(now.getMonth());
+
+// console.log(now.getDay());
+
+//0 воскресенье 
+
+//метожы set
+
+console.log(now.setFullYear(200));
 console.log(now)
 
-console.log(now.getFullYear());
+
+
