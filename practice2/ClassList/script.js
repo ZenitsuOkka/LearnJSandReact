6695b770d81b1b4c9920911bab9d5d3879a2ce44
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-25 14:40:43 +0300

    createElement delegation

--- practice2/ClassList/script.js
+++ practice2/ClassList/script.js
@@ -21,8 +21,12 @@ btns[0].addEventListener('click', () => {
 });
 
 wrapper.addEventListener('click', (event) => {
-    console.dir(event.target);
+    // console.dir(event.target);
     if (event.target && event.target.tagName == "BUTTON") {
         console.log('Hello')
     }
 });
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
