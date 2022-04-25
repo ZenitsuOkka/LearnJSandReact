155511242d1eb3bed88b25fad8418a494f7b5918
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-25 16:05:27 +0300

    tabs chang

--- practice2/Food-dist/js/script.js
+++ practice2/Food-dist/js/script.js
@@ -22,4 +22,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
     hideTabContent();
     showTabContent();
+
+    tabsPerent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            })
+        }
+    });
 });
