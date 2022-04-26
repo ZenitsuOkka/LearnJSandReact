63097b5c40197e365c1c8a707e45fa3760ee8932
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-26 14:22:20 +0300

    preview offsetWidth/Height

--- practice2/preview/script.js
+++ practice2/preview/script.js
@@ -2,7 +2,9 @@
 
 const box = document.querySelector('.box');
 
-const width = box.clientWidth;
-const height = box.clientHeight;
+// const width = box.clientWidth;
+// const height = box.clientHeight;
+const width = box.offsetWidth;
+const height = box.offsetHeight;
 
 console.log(width, height);
