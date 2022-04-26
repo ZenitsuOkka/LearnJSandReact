0303082711c3f7c421d0164f2a2eb7f88de9be9f
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-26 20:27:38 +0300

    this in obj

--- practice2/This/script.js
+++ practice2/This/script.js
@@ -1,14 +1,25 @@
 'use strict';
 
 //функция может вызываться 4 разными способами в зависимости от контекста
-function showThis(a, b) {
-    console.log(this);
-    function sum() {
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this)
+//         return a + b;
+//     }
+//     console.log(sum());
+// }
+// showThis(4, 5);
+
+const obj = {
+    a: 20,
+    b: 15,
+    sum: function() {
         console.log(this)
-        return a + b;
     }
-    console.log(sum());
-}
-showThis(4, 5);
+};
+
+obj.sum();
 
 // 1) Oбычная функция this = window, но если 'use strict'; - undefined
+// 2)
