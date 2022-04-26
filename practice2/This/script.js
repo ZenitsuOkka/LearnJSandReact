7d09b60b83d6a2c1a2d835cf5cf885c40678fe51
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-26 20:24:50 +0300

    This a and this b

---
+++ practice2/This/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//функция может вызываться 4 разными способами в зависимости от контекста
+function showThis(a, b) {
+    console.log(this);
+    function sum() {
+        console.log(this)
+        return this.a + this.b;
+    }
+    console.log(sum());
+}
+showThis(4, 5);
+
+// 1) Oбычная функция this = window, но если 'use strict'; - undefined
