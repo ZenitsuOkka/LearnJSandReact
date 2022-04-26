7a92e61e5b34b1248842a010fb7d37320adb65d9
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-26 20:35:35 +0300

    this in constructor or class

--- practice2/This/script.js
+++ practice2/This/script.js
@@ -11,18 +11,26 @@
 // }
 // showThis(4, 5);
 
-const obj = {
-    a: 20,
-    b: 15,
-    sum: function() {
-        function shout() {
-            console.log(this);
-        }
-        shout();
-    }
-};
+// const obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         function shout() {
+//             console.log(this);
+//         }
+//         shout();
+//     }
+// };
 
-obj.sum();
+// obj.sum();
+
+function User(name, id) {
+    this.name = name;//this обозначает что у каждого нового обьекта будет свое значение имени и идентификатора
+    this.id = id;
+    this.human = true;//после использования такого синтаксиса наша функция стало конструктор и с помощью его мы можем создаватьновые обьекты
+}
+
+let ivan = new User('Ivan', 23)
 
 // 1) Oбычная функция this = window, но если 'use strict'; - undefined
 // 2) Контекст у методов обьекта это будет сам обьект
