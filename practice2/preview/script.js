120cedd8aaf692aaf32c4dd278ba57b0a41a93f9
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-26 14:24:04 +0300

    preview csrollWidth/Height

--- practice2/preview/script.js
+++ practice2/preview/script.js
@@ -4,7 +4,11 @@ const box = document.querySelector('.box');
 
 // const width = box.clientWidth;
 // const height = box.clientHeight;
-const width = box.offsetWidth;
-const height = box.offsetHeight;
+
+// const width = box.offsetWidth;
+// const height = box.offsetHeight;
+
+const width = box.scrollWidth;
+const height = box.scrollHeight;
 
 console.log(width, height);
