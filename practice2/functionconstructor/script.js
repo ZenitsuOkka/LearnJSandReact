9b21edbb5280b32c887b8e2ff4019149b9131f6f
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-26 19:43:50 +0300

    this method

--- practice2/functionconstructor/script.js
+++ practice2/functionconstructor/script.js
@@ -1,7 +1,12 @@
 'use strict';
 
-const num = new Number(3); 
-const num1 = new Function(3); 
+// const num = new Number(3); 
+// const num1 = new Function(3); 
 
-console.log(num)
-console.log(num1)
+// console.log(num)
+// console.log(num1)
+
+function User(name, id) {
+    this.name = name;//this обозначает что у каждого нового обьекта будет свое значение имени и идентификатора
+    this.id = id;
+}
