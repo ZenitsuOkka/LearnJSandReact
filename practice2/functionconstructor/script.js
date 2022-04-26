b1de0d66b58f60a2beb0ae73f304f4221e48618c
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-04-26 20:15:58 +0300

    This method function

--- practice2/functionconstructor/script.js
+++ practice2/functionconstructor/script.js
@@ -8,20 +8,20 @@
 
 
 
-class User {
-    constructor User(name, id) {
-        this.name = name;//this обозначает что у каждого нового обьекта будет свое значение имени и идентификатора
-        this.id = id;
-        this.human = true;//после использования такого синтаксиса наша функция стало конструктор и с помощью его мы можем создаватьновые обьекты
-    }
-    hello() {
-        console.log(`Hello ${this.name}`)
-    }
-    exit() {
-        console.log(`Пользователь ${this.name} ушел`);
-    }
+// class User {
+//     constructor User(name, id) {
+//         this.name = name;//this обозначает что у каждого нового обьекта будет свое значение имени и идентификатора
+//         this.id = id;
+//         this.human = true;//после использования такого синтаксиса наша функция стало конструктор и с помощью его мы можем создаватьновые обьекты
+//     }
+//     hello() {
+//         console.log(`Hello ${this.name}`)
+//     }
+//     exit() {
+//         console.log(`Пользователь ${this.name} ушел`);
+//     }
 
-}
+// }
 
 // User.prototype.exit = function() {
 //     console.log(`Пользователь ${this.name} ушел`);
