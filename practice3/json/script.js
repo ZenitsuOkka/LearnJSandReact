810bfd7cd7fdb0ad4718ef7e2fb15dc92ac70e03
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-05-09 15:46:05 +0300

    json practice clone

--- practice3/json/script.js
+++ practice3/json/script.js
@@ -2,8 +2,14 @@
 
 const persone = {
     name: 'Alex',
-    tel: '0997349056'
+    tel: '0997349056',
+    parents: {
+        mom: 'Olga',
+        dad: 'Mike'
+    }
 };  
 
 // console.log(JSON.stringify(persone));
-console.log(JSON.parse(JSON.stringify(persone)));
+// console.log(JSON.parse(JSON.stringify(persone)));
+const clone = JSON.parse(JSON.stringify(persone));
+
