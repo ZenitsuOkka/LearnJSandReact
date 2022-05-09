5c59134463d540bf203c3ea5404d1e11a5473cd7
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-05-09 17:34:11 +0300

    ajax practice calc const data

--- practice3/ajax/calc1/js/script.js
+++ practice3/ajax/calc1/js/script.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const inputUah = document.querySelector('#uah'),
-      inputUsd = document.querySelector('#usd');
+const inputURub = document.querySelector('.#rub'),
+      inputUsd = document.querySelector('.#usd');
 
 
-inputUah.addEventListener('input', () => {
+inputRub.addEventListener('input', () => {
     const request = new XMLHttpRequest();
 
     request.open('GET', 'js/current.json');
