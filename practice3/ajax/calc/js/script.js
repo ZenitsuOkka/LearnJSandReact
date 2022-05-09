51192d571f4f10082bb49bbbffc216a59bab53e9
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-05-09 16:59:06 +0300

    ajax practice calc event readystatechange

--- practice3/ajax/calc/js/script.js
+++ practice3/ajax/calc/js/script.js
@@ -8,5 +8,17 @@ inputUah.addEventListener('input', () => {
     const request = new XMLHttpRequest();
 
     request.open('GET', 'js/current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8'); 
+    request.send();
+
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.response);
+        }
+    });
+    // status()
+    // statusText()
+    // response
+    //readyState
+
 });
