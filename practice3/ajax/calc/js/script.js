e4c7e2268b8629d0dd6dd6ad4b3e6241c9fe90cc
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-05-09 16:21:12 +0300

    ajax practice calc request set

--- practice3/ajax/calc/js/script.js
+++ practice3/ajax/calc/js/script.js
@@ -2,3 +2,11 @@
 
 const inputUah = document.querySelector('.#uah'),
       inputUsd = document.querySelector('.#usd');
+
+
+inputUah.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+});
