cf72e3db3ae8e348357fa3bd9caeadaffeef8112
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-05-10 21:02:06 +0300

    massive.reduce

--- practice3/massive/script.js
+++ practice3/massive/script.js
@@ -27,8 +27,33 @@
 
 // every/some
 
-const per = [5, 6, 'andrey'];
+// const per = [5, 6, 'andrey'];
 
-console.log(per.some(item => typeof(item) === 'number'));
+// console.log(per.some(item => typeof(item) === 'number'));
 
-console.log(per.every(item => typeof(item) ==='number'));
+// console.log(per.every(item => typeof(item) ==='number'));
+
+
+// reduce
+
+// const arr = [1, 22, 5, 6, 8, 12];
+//                             // 0       1
+//                             // 1       22
+//                             // 23      5
+//                             // 28      6
+//                             // 34      8
+//                             // 42      12
+//                             // 54      0
+
+// const result = arr.reduce((sum, current) => sum + current);
+
+// console.log(result)
+
+const arr = ['1, 22, 5, 6, 8, 12', 'apple', 'yellow'];
+
+const result1 = arr.reduce((sum, current) => sum + ', '+ current);
+const result2 = arr.reduce((sum, current) => `${sum}, ${current}`);
+
+
+console.log(result1)
+console.log(result2)
