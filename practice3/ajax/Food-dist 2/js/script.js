e64281061be8d6f683fb7976648700533d0e34df
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-05-14 21:27:29 +0300

    slider prev next total current

--- practice3/ajax/Food-dist 2/js/script.js
+++ practice3/ajax/Food-dist 2/js/script.js
@@ -286,12 +286,21 @@ window.addEventListener('DOMContentLoaded', function() {
 
     const slides = document.querySelectorAll('.offer__slide'),
           prev = document.querySelector('.offer__slider-prev'),
-          next = document.querySelector('.offer__slider-next');
+          next = document.querySelector('.offer__slider-next'),
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current');
+
 
         
     let slideIndex = 1;
 
     showSlides(slideIndex);
+
+    if(slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
     
     function showSlides(n) {
         if (n > slides.length) {
@@ -305,6 +314,12 @@ window.addEventListener('DOMContentLoaded', function() {
         slides.forEach((item) => item.style.display = 'none');
 
         slides[slideIndex - 1].style.display = 'block';
+
+        if(slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
 
     }
 
