eccca74f03ca46a70fc4ee4cae200c95453bfda1
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-05-16 22:14:35 +0300

    localstorage practice

---
+++ practice3/ajax/preview/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// localStorage.setItem('number', 5);
+
+// console.log(localStorage.getItem('number'));
+
+const checkbox = document.querySelector('#checkbox'),
+      form = documnet.querySelector('form'),
+      change = document.querySelector('#color');
+
+
+
