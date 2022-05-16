9822d6487053d3c209092cc6a88b98f75e93c8e7
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-05-16 22:27:39 +0300

    localstorage practice change color

--- practice3/ajax/preview/script.js
+++ practice3/ajax/preview/script.js
@@ -11,8 +11,21 @@ const checkbox = document.querySelector('#checkbox'),
 if (localStorage.getItem('isChecked')) {
     checkbox.checked = true;
 }
+if (localStorage.getItem('bg') === 'changed') {
+    form.style.backgroundColor = 'red';
+}
 
 checkbox.addEventListener('change', () => {
     localStorage.setItem('isChecked', true);
 });
 
+change.addEventListener('click', () => {
+    if (localStorage.getItem('bg') === 'changed') {
+        localStorage.removeItem('bg');
+        form.style.backgroundColor = '#fff';
+
+    } else {
+        localStorage.setItem('bg', 'changed');
+        form.style.backgroundColor = 'red';
+    }
+});
