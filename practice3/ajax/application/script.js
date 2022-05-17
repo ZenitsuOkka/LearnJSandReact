ddc9638a1f3392596221a4b7f10f0bda0031bcd7
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-05-17 20:40:05 +0300

    regular variables replace *

--- practice3/ajax/application/script.js
+++ practice3/ajax/application/script.js
@@ -3,11 +3,15 @@
 // new RegExp('pattern', 'flags');
 // /pattern/f
 
-const ans = prompt('Your name?');
+// const ans = prompt('Your name?');
 
-const reg = /n/i;
+const reg = /n/ig;
 // i
 // g
 // m
 
-console.log(ans.match(reg));
+// console.log(ans.match(reg));
+const pass = prompt('Your password?');
+
+console.log(pass.replace(/./, "*"));
+
