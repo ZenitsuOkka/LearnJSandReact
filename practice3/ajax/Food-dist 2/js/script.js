fe6d1d6025e5132fb1dad97345f5301adfd88a68
ZenitsuOkka <zenitsu.okka@gmail.com>
2022-05-29 13:10:17 +0300

    import js correct

--- practice3/ajax/Food-dist 2/js/script.js
+++ practice3/ajax/Food-dist 2/js/script.js
@@ -1,10 +1,10 @@
-import tabs './modules/tabs';
-import modal './modules/modal';
-import calc './modules/calc';
-import cards './modules/cards';
-import forms './modules/forms';
-import slider './modules/slider';
-import timer './modules/timer';
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import calc from './modules/calc';
+import cards from './modules/cards';
+import forms from './modules/forms';
+import slider from './modules/slider';
+import timer from './modules/timer';
 
 window.addEventListener('DOMContentLoaded', function() {
                
